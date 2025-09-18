81ae68b9133f8214ae3d52c2160f0b60a16b293e
Frederic Richard <84329965+fjprichard@users.noreply.github.com>
2025-09-18 15:25:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Scientific/Engineering :: Image Processing',
     ],
     project_urls={
